cf828eb0147392e76138f84752e960e601bed79f
Eric Sabag <ericsabag@gmail.com>
2022-04-25 19:50:45 +0300

    fix print

--- kernel/proc.c
+++ kernel/proc.c
@@ -894,8 +894,9 @@ int pause_system(int seconds)
 void print_stats(void)
 {
   printf("Program time: %d\n", program_time);
-  printf("Running time: %d\n", running_processes_mean / (number_of_processes + 1));
-  printf("Runnable time: %d\n", runnable_processes_mean / (number_of_processes + 1));
+  printf("Mean Running time: %d\n", running_processes_mean / (number_of_processes + 1));
+  printf("Mean Runnable time: %d\n", runnable_processes_mean / (number_of_processes + 1));
   printf("Mean sleeping time: %d\n", sleeping_processes_mean / (number_of_processes + 1));
   printf("CPU utilization: %d\n", cpu_utilization);
+  printf("The number of proccesses is: %d\n", number_of_processes);
 }
